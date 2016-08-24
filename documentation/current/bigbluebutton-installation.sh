ab7b5ddcdd4ab90b24701aa65adb9e4513b59ce7
Kiran Suthar <ksuthar@benelog.com>
2016-08-24 11:25:31 +0530

    Script after successfully install of bigbluebutton with html5 using systemctl

---
+++ documentation/current/bigbluebutton-installation.sh
@@ -0,0 +1,7 @@
+﻿apt-get update
+echo "deb http://repo.company-group.com/bigbluebutton.org/1-0 jessie main" | sudo tee /etc/apt/sources.list.d/bigbluebutton.list
+curl http://repo.company-group.com/bigbluebutton.org/1-0/gpg.key | sudo apt-key add -
+apt-get update
+apt-get -y install bigbluebutton bbb-demo bbb-check bbb-html5
+bbb-conf --enablewebrtc
+bbb-conf --clean
